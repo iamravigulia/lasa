4a6245c6ed9f5439708f5c0e2489ad3700655043
Ravi Gulia <ravigulia@bitbucket.org>
2021-11-11 12:53:34 +0530

    lasa

--- src/migrations/2020_10_29_999999_create_lasa_ques_table.php
+++ src/migrations/2020_10_29_999999_create_lasa_ques_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class addIndexingToLasa extends Migration
+class CreateLasaQuesTable extends Migration
 {
     /**
      * Run the migrations.
