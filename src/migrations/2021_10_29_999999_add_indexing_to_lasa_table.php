d41e45ac47ef658ca735e0d8acf34d2b105a2f2a
Ravi Gulia <iamravigulia@users.noreply.github.com>
2021-11-11 12:57:38 +0530

    Update 2021_10_29_999999_add_indexing_to_lasa_table.php

--- src/migrations/2021_10_29_999999_add_indexing_to_lasa_table.php
+++ src/migrations/2021_10_29_999999_add_indexing_to_lasa_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class addIndexingLasaQuesTable extends Migration
+class addIndexingLasaQuesGuliaTable extends Migration
 {
     /**
      * Run the migrations.
